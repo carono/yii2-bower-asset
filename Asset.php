6b2df5f01bc8584b872d7ff088898d36e9e6aa64
carono <info@carono.ru>
2017-08-14 13:03:49 +0500

    Кеширование асетсов, устанавливается зависимость создаваемых классов, от отсновного по времери изменения файла.

--- Asset.php
+++ Asset.php
@@ -3,6 +3,7 @@
 
 namespace carono\yii2bower;
 
+use yii\helpers\ArrayHelper;
 use yii\helpers\StringHelper;
 use yii\helpers\Inflector;
 use yii\helpers\VarDumper;
@@ -16,11 +17,13 @@ class Asset extends AssetBundle
 
     public function init()
     {
+        $timestamp = $this->getFileTimestamp(self::getClassFile(get_class($this)));
         foreach ($this->packages as $idx => $value) {
             $package = is_numeric($idx) ? $value : $idx;
             $files = is_numeric($idx) ? [] : $value;
-            if (!$this->assetExists($package)) {
+            if (!$this->assetExists($package) || $this->needRewrite($package)) {
                 self::createAssetClass($package, $this->packageNamespace, $files);
+                touch(self::getPackageFile($package, $this->packageNamespace), $timestamp);
             };
             $this->depends[] = self::getPackageClass($package, $this->packageNamespace);
         }
@@ -49,26 +52,42 @@ class Asset extends AssetBundle
     /**
      * @param $package
      * @param $namespace
+     * @return string
+     */
+    protected static function getPackageFile($package, $namespace)
+    {
+        return self::getClassFile(self::getPackageClass($package, $namespace));
+    }
+
+    /**
+     * @param $package
+     * @param $namespace
      * @param array $files
      */
     public static function createAssetClass($package, $namespace, $files = [])
     {
-        $bowerJson = \Yii::getAlias('@bower') . '/' . $package . '/bower.json';
-        $json = json_decode(file_get_contents($bowerJson), true);
+        $dir = \Yii::getAlias("@bower/$package");
+        $bowerJson = "$dir/bower.json";
+        if (file_exists($bowerJson)) {
+            $json = json_decode(file_get_contents($bowerJson), true);
+            $main = ArrayHelper::getValue($json, 'main', []);
+        } else {
+            $main = [];
+        }
         $js = [];
         $css = [];
-        $main = $files ? $files : (is_array($json['main']) ? $json['main'] : [$json['main']]);
+        $main = $files ? $files : (is_array($main) ? $main : [$main]);
         foreach ($main as $file) {
             if (StringHelper::endsWith($file, '.css')) {
-                $css[] = $file;
+                $css[] = ltrim($file, "./\\");
             }
             if (StringHelper::endsWith($file, '.js')) {
-                $js[] = $file;
+                $js[] = ltrim($file, "./\\");
             }
         }
         $class = self::formClassNameFromPackage($package);
-        $js = VarDumper::dumpAsString($js);
-        $css = VarDumper::dumpAsString($css);
+        $js = VarDumper::export($js);
+        $css = VarDumper::export($css);
         $template = <<<PHP
 <?php
 
@@ -104,5 +123,40 @@ PHP;
     protected function assetExists($package)
     {
         return class_exists(self::getPackageClass($package, $this->packageNamespace));
+    }
+
+    /**
+     * @param $package
+     * @return bool
+     */
+    protected function needRewrite($package)
+    {
+        $timestamp = $this->getFileTimestamp(self::getPackageFile($package, $this->packageNamespace));
+        if ($timestamp) {
+            $assetTimestamp = $this->getFileTimestamp(self::getClassFile(get_class($this)));
+            return $timestamp != $assetTimestamp;
+        }
+        return false;
+    }
+
+    /**
+     * @param $class
+     * @return string
+     */
+    protected static function getClassFile($class)
+    {
+        return \Yii::getAlias('@' . ltrim(str_replace('\\', '/', $class), '/')) . '.php';
+    }
+
+    /**
+     * @param $file
+     * @return bool|int
+     */
+    protected function getFileTimestamp($file)
+    {
+        if ($timestamp = @filemtime($file)) {
+            $timestamp = intval($timestamp / 100) * 100;
+        }
+        return $timestamp;
     }
 }
